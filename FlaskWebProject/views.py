eae93aaaafaa0c4d398f50009bde6202e599bcae
Shen <slee051@e.ntu.edu.sg>
2017-02-16 17:49:54 +0800

    updated pages

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -7,24 +7,30 @@ from flask import render_template, request, url_for
 from FlaskWebProject import app
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['POST'])
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-    )
-	
-@app.route('/add', methods=['POST'])
-def add():
-	"""Renders the addition sum page"""
 	a=request.form['int_a']
 	b=request.form['int_b']
 	c=int(a)+int(b)
-	return render_template(
-		'add.html',
-		title='Addition',
+    return render_template(
+        'index.html',
+        title='Home Page',
 		a=a,
 		b=b,
 		sum=c,
-	)
+    )
+	
+#@app.route('/add', methods=['POST'])
+#def add():
+#	"""Renders the addition sum page"""
+#	a=request.form['int_a']
+#	b=request.form['int_b']
+#	c=int(a)+int(b)
+#	return render_template(
+#		'add.html',
+#		title='Addition',
+#		a=a,
+#		b=b,
+#		sum=c,
+#	)
