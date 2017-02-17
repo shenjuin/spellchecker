debab029e1a6e0d816186d40cad3bd8eb618493f
Shen <slee051@e.ntu.edu.sg>
2017-02-17 12:59:43 +0800

    updated views

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -6,7 +6,3 @@ from flask import Flask
 app = Flask(__name__)
 
 import FlaskWebProject.views
-
-def autocorrect(word):
-    """Checks if input word is in corpus: if not, measures word distance and provides nearest word suggestions (if any)"""
-	return word+"s"
