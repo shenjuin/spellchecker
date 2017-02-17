b774004c0c1fc30f91eae95c5f782495c9c46ddd
Shen <slee051@e.ntu.edu.sg>
2017-02-17 12:57:19 +0800

    updated pages

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -18,7 +18,6 @@ def home():
 def check():
 	"""Renders the spellcheck result page"""
 	word=request.form['word']
-	word=autocorrect(word)
 	return render_template(
 		'check.html',
 		title='Spell Check',
